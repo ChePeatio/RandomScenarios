d94cbbcf56e43c8e86e5d92093dfd3a9b75478cd
Chepeatio <chepeatio@hotmail.com>
2015-11-30 01:07:32 +0800

    基本逻辑已经完成，明天部署，让用户使用，有问题再进一步完善

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ var _    = require('lodash');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: '随机场景系统', error: req.flash('error').toString()});
+	var user = req.session.user;
+	res.render('index', { title: '随机场景系统', error: req.flash('error').toString(), user: user});
 });
 
 // normal user login the system
@@ -51,127 +52,147 @@ router.post('/',function (req,res,next) {
 // System Manager Login
 //router.get('/login', checkNotLogin);
 router.get('/login',function (req,res,next) {
-	res.render('login', { title: '系统管理员登录' });
+	var user = null;
+	res.render('login', { title: '系统管理员登录', error: req.flash('error').toString(), user: user});
 });
 
 // system manager login the system
 router.post('/login',function (req,res,next) {
-	res.redirect('/userlist');
+	if (req.body.username!="xiadaadmin" || req.body.password!="zxcvbnm7890") {
+		req.flash('error', "用户名、口令错误！");
+		res.redirect('/login');
+	} else {
+		req.session.systemadmin = req.body.username;
+		req.flash('success', "系统管理员登录成功");
+		res.redirect('/userlist');
+	}
 });
 
 
 // Random Scenario 1
-//router.get('/scenario1', checkNotLogin);
 router.get('/scenario1',function (req,res,next) {
 	var username = req.session.user;
 	if (username) {
-	console.log(username);
-	UserModel.findOne({'name':username}).exec(function (err, user) {
-		if (err) {
-			console.log(err);
-			next(err);
-		}
-		console.log(user);
-        var pos = user.s1;
-		console.log(pos);
+		//console.log(username);
+		UserModel.findOne({'name':username}).exec(function (err, user) {
+			if (err) {
+				console.log(err);
+				next(err);
+			}
+		//console.log(user);
+		var pos = user.s1;
+		//console.log(pos);
 		ScenarioModel.find().exec(function (err, scenarios) {
-        	if (err) {
-            	next(err);
-        	}
-        	var scen = [];
-        	for (var i=0; i<scenarios.length; i++) {
-        		for (var j=0; j<scenarios.length; j++) {
-        			if (i == scenarios[j].id) {
-        				scen[i] = {'timu':scenarios[j].name, 'xuanze':scenarios[j].scenario[pos[i]]};
-        				break;
-        			}
-        		}
-        	}
-        	res.render('scenario1', { title: '随机场景1', scen: scen });
-    	});
+			if (err) {
+				next(err);
+			}
+			var scen = [];
+			for (var i=0; i<scenarios.length; i++) {
+				for (var j=0; j<scenarios.length; j++) {
+					if (i == scenarios[j].id) {
+						scen[i] = {'timu':scenarios[j].name, 'xuanze':scenarios[j].scenario[pos[i]]};
+						break;
+					}
+				}
+			}
+			res.render('scenario1', { title: '随机场景1', scen: scen, user:username });
+		});
 	});	
-} else {
-	req.flash('error', "登录超时");
-	res.redirect('/');
-}
+	} else {
+		req.flash('error', "未登录或登录超时");
+		res.redirect('/');
+	}
 });
 
 // Random Scenario 2
-//router.get('/scenario2', checkNotLogin);
 router.get('/scenario2',function (req,res,next) {
 	var username = req.session.user;
 	if (username) {
-		console.log(username);
-	UserModel.findOne({'name':username}).exec(function (err, user) {
-		if (err) {
-			console.log(err);
-			next(err);
-		}
-		console.log(user);
-        var pos = user.s2;
-		console.log(pos);
-		ScenarioModel.find().exec(function (err, scenarios) {
-        	if (err) {
-            	next(err);
-        	}
-        	var scen = [];
-        	for (var i=0; i<scenarios.length; i++) {
-        		for (var j=0; j<scenarios.length; j++) {
-        			if (i == scenarios[j].id) {
-        				scen[i] = {'timu':scenarios[j].name, 'xuanze':scenarios[j].scenario[pos[i]]};
-        				break;
-        			}
-        		}
-        	}
-        	res.render('scenario2', { title: '随机场景2', scen: scen });
-    	});
-	});	
-} else {
-	req.flash('error', "登录超时");
-	res.redirect('/');
-}
+		//console.log(username);
+		UserModel.findOne({'name':username}).exec(function (err, user) {
+			if (err) {
+				console.log(err);
+				next(err);
+			}
+			//console.log(user);
+			var pos = user.s2;
+			//console.log(pos);
+			ScenarioModel.find().exec(function (err, scenarios) {
+				if (err) {
+					next(err);
+				}
+				var scen = [];
+				for (var i=0; i<scenarios.length; i++) {
+					for (var j=0; j<scenarios.length; j++) {
+						if (i == scenarios[j].id) {
+							scen[i] = {'timu':scenarios[j].name, 'xuanze':scenarios[j].scenario[pos[i]]};
+							break;
+						}
+					}
+				}
+				res.render('scenario2', { title: '随机场景2', scen: scen, user:username });
+			});
+		});	
+	} else {
+		req.flash('error', "未登录或登录超时");
+		res.redirect('/');
+	}
 });
 
 // Random Scenario 3
-//router.get('/scenario3', checkNotLogin);
 router.get('/scenario3',function (req,res,next) {
 	var username = req.session.user;
 	if (username) {
-	console.log(username);
-	UserModel.findOne({'name':username}).exec(function (err, user) {
-		if (err) {
-			console.log(err);
-			next(err);
-		}
-		console.log(user);
-        var pos = user.s3;
-		console.log(pos);
-		ScenarioModel.find().exec(function (err, scenarios) {
-        	if (err) {
-            	next(err);
-        	}
-        	var scen = [];
-        	for (var i=0; i<scenarios.length; i++) {
-        		for (var j=0; j<scenarios.length; j++) {
-        			if (i == scenarios[j].id) {
-        				scen[i] = {'timu':scenarios[j].name, 'xuanze':scenarios[j].scenario[pos[i]]};
-        				break;
-        			}
-        		}
-        	}
-        	res.render('scenario3', { title: '随机场景3', scen: scen });
-    	});
-	});	
-} else {
-	req.flash('error', "登录超时");
-	res.redirect('/');
-}
+		//console.log(username);
+		UserModel.findOne({'name':username}).exec(function (err, user) {
+			if (err) {
+				console.log(err);
+				next(err);
+			}
+			//console.log(user);
+			var pos = user.s3;
+			//console.log(pos);
+			ScenarioModel.find().exec(function (err, scenarios) {
+				if (err) {
+					next(err);
+				}
+				var scen = [];
+				for (var i=0; i<scenarios.length; i++) {
+					for (var j=0; j<scenarios.length; j++) {
+						if (i == scenarios[j].id) {
+							scen[i] = {'timu':scenarios[j].name, 'xuanze':scenarios[j].scenario[pos[i]]};
+							break;
+						}
+					}
+				}
+				res.render('scenario3', { title: '随机场景3', scen: scen, user:username });
+			});
+		});	
+	} else {
+		req.flash('error', "未登入或登录超时");
+		res.redirect('/');
+	}
 });
 
 
 // User List with their random sequence
 router.get('/userlist',function (req, res, next) {
-	res.render('userlist', { title: '当前系统用户列表' });
+	var systemadmin = req.session.systemadmin;
+	if (systemadmin) {
+		UserModel.find().exec(function (err, usersinfo) {
+			if (err) {
+				console.log(err);
+				next(err);
+			}
+			var list = usersinfo.map(function(one) {
+				return {'name':one.name, 's1':one.s1, 's2':one.s2, 's3':one.s3}
+			});
+			res.render('userlist', { title: '当前系统用户列表', user:null, list:list });
+		});
+	} else {
+		req.flash('error', "未登入或登录超时");
+		res.redirect('/login');
+	}
 });
 
 
